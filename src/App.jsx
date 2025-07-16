fe4a9610ec0d0a4a8318dccaf72afdd2638b02db
Saidu Kamara <mhsaidu@gmail.com>
2025-07-16 14:29:39 -0400

    landing page is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { useState } from 'react'
-import { Routes } from 'react-router-dom'
+// import { useState } from 'react'
+import { Routes,Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
 
 const App = () => {
 
   return (
     <>
       <Routes>
-
+        <Route path="/" element={<LandingPage />} /> 
         </Routes>
     </>
   )
 }
+export default App;
